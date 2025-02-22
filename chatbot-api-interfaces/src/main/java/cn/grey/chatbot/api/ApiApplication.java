85532ee0db0d806f15e3244c155e63433efa89ed
雨碎风希 <2078389729@qq.com>
2025-02-23 00:18:48 +0800

    init ddd

---
+++ chatbot-api-interfaces/src/main/java/cn/grey/chatbot/api/ApiApplication.java
@@ -0,0 +1,19 @@
+package cn.grey.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: ApiApplication
+ * Description:
+ *
+ * @Author: shenjiaqi
+ * 编辑于：2025/2/22 23:46   @Version 1.0        描述
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/grey/chatbot/api/test/ApiTest.java
@@ -0,0 +1,12 @@
+package cn.grey.chatbot.api.test;
+
+/**
+ * ClassName: ApiTest
+ * Description:
+ *
+ * @Author: shenjiaqi
+ * 编辑于：2025/2/23 00:10   @Version 1.0        描述
+ */
+public class ApiTest {
+
+}
